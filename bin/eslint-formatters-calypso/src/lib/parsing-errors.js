4e2459a4743764d6b82fbdd806c775635579e666
nssw <nssw@users.noreply.github.com>
2016-07-28 15:20:15 +0200

    Properly check for parsing errors

--- bin/eslint-formatters-calypso/src/lib/parsing-errors.js
+++ bin/eslint-formatters-calypso/src/lib/parsing-errors.js
@@ -7,30 +7,10 @@ module.exports = function( report ) {
 	// to tweak and edit in place - as this function remains pure.
 	let newReport = [];
 
-	const isParsingError = ( message ) => {
-		// ESLint team have reached the consensus of treating
-		// parsing errors as any other error. See:
-		//
-		// - discussion https://github.com/eslint/eslint/issues/3555
-		// - PR https://github.com/eslint/eslint/pull/3967
-		//
-		// Unlike any other error, thought, its ruleId would be null
-		// and the message received by the parser would be prepended
-		// by the string 'Parsing error:'. Also, they would contain
-		// a new key "fatal" with value true.
-
-		if ( ( message.ruleId === null ) ||
-		message.message.startsWith( 'Parsing error:' ) ||
-		message.fatal ) {
-			return true;
-		}
-		return false;
-	};
-
 	report.forEach( file => {
 		let areParsingErrors = false;
 		file.messages.forEach( message => {
-			if ( isParsingError( message ) ) {
+			if ( message.fatal ) {
 				// A file contains only 1 message if there are parsing errors
 				// so it would be safe to push the file to the report
 				// at this point. Just in case, use a flag a push it outside
--- bin/eslint-formatters-calypso/tests/test-parsing-errors.js
+++ bin/eslint-formatters-calypso/tests/test-parsing-errors.js
@@ -2,11 +2,8 @@ const test = require( 'tape' );
 const parsingErrors = require( '../src/lib/parsing-errors.js' );
 
 // fixtures
-const reportWithoutParsingErrors = require( './fixtures/eslint.json' );
 const reportWithParsingError = require( './fixtures/eslint-parsing-errors.json' );
-const reportWithParsingErrorMsg = require( './fixtures/eslint-parsing-errors-only-message.json' );
-const reportWithParsingErrorRule = require( './fixtures/eslint-parsing-errors-only-ruleid.json' );
-const reportWithParsingErrorFatal = require( './fixtures/eslint-parsing-errors-only-fatal.json' );
+const reportWithoutParsingErrors = require( './fixtures/eslint.json' );
 
 test( 'parsing-errors - returns empty report for reports without parsing errors', t => {
 	const newReport = parsingErrors( reportWithoutParsingErrors );
@@ -18,15 +15,6 @@ test( 'parsing-errors - returns empty report for reports without parsing errors'
 test( 'parsing errors - returns report containing only files with parsing errors', t => {
 	const newReport = parsingErrors( reportWithParsingError );
 	t.equals( newReport.length, 1 );
-
-	const newReportMsg = parsingErrors( reportWithParsingErrorMsg );
-	t.equals( newReportMsg.length, 1 );
-
-	const newReportRule = parsingErrors( reportWithParsingErrorRule );
-	t.equals( newReportRule.length, 1 );
-
-	const newReportFatal = parsingErrors( reportWithParsingErrorFatal );
-	t.equals( newReportFatal.length, 1 );
 
 	t.end();
 } );
