d76175a584208a2afdfbee8087216115ab6d0ee4
nssw <nssw@users.noreply.github.com>
2016-07-31 16:53:59 +0200

    allow eslines to be passed the branch to choose the formatter
    If the env variable ESLINES_BRANCH exists, it would be used to determine which formatter to run.
    If not, git would be used instead to determine the name of the branch in use.

--- bin/eslint-formatters-calypso/src/formatters/eslines.js
+++ bin/eslint-formatters-calypso/src/formatters/eslines.js
@@ -1,10 +1,18 @@
+/* eslint strict: "off" */
+
+'use strict';
+
 const childProcess = require( 'child_process' );
 const linesAndRules = require( './lines-modified-and-specific-rules' );
 const parsingErrors = require( './parsing-errors' );
 
 module.exports = function( report ) {
-	const argsBranchName = [ 'rev-parse', '--abbrev-ref', 'HEAD' ];
-	const branchName = childProcess.spawnSync( 'git', argsBranchName ).stdout.toString().trim();
+
+	let branchName = process.env.ESLINES_BRANCH;
+	if( ! branchName ){
+		const argsBranchName = [ 'rev-parse', '--abbrev-ref', 'HEAD' ];
+		branchName = childProcess.spawnSync( 'git', argsBranchName ).stdout.toString().trim();
+	}
 
 	if ( branchName === 'master' ) {
 		return parsingErrors( report );
