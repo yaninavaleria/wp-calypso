7a935e801b7c3d9310ac5534de00daa0493baba9
nssw <nssw@users.noreply.github.com>
2016-07-31 19:39:34 +0200

    Let eslines to be passed an env variable to calculate how lines modified are constructed.
    If ESLINES_DIFF='index' is set, eslines would calculate the lines modified based on the files in the git index.
    If it is not, lines modified would be constructed comparing the current branch with origin/master.

--- bin/eslint-formatters-calypso/src/formatters/lines-modified-and-specific-rules.js
+++ bin/eslint-formatters-calypso/src/formatters/lines-modified-and-specific-rules.js
@@ -1,11 +1,24 @@
+/* eslint strict: "off" */
+
+'use strict';
+
 var fs = require( 'fs' );
 const differ = require( '../lib/differ' );
 const eslines = require( '../lib/eslines' );
-const gitDiff = require( '../lib/git-diff' );
+const gitDiffBranchVSMaster = require( '../lib/git-diff' );
+const gitDiffIndex = require( '../lib/git-diff-index' );
 
 const config = JSON.parse( fs.readFileSync( '.eslines.json', 'utf-8' ) );
 
 module.exports = function( report ) {
-	const lines = differ( gitDiff() );
+	const whatToDiff = process.env.ESLINES_DIFF;
+	let diff;
+	if(whatToDiff === 'index'){
+		diff = gitDiffIndex();
+	} else {
+		diff = gitDiffBranchVSMaster();
+	}
+
+	const lines = differ( diff );
 	return JSON.stringify( eslines( report, lines, config.rulesToNotDowngrade ) );
 };
