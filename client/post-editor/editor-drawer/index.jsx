dca254abd7010c469230f0fb495e58bea63473b6
Copons <copons@users.noreply.github.com>
2016-08-08 13:32:24 +0200

    Add the Copy Post accordion to the Editor Drawer

--- client/post-editor/editor-drawer/index.jsx
+++ client/post-editor/editor-drawer/index.jsx
@@ -257,6 +257,18 @@ const EditorDrawer = React.createClass( {
 		);
 	},
 
+	renderCopyPost: function() {
+		const { type, post, site } = this.props;
+
+		return (
+			<CopyPostAccordion
+				type={ type }
+				post={ post }
+				site={ site }
+			/>
+		);
+	},
+
 	renderMoreOptions: function() {
 		if (
 			! this.currentPostTypeSupports( 'excerpt' ) &&
@@ -307,6 +319,7 @@ const EditorDrawer = React.createClass( {
 				{ this.renderSharing() }
 				{ this.renderPostFormats() }
 				{ this.renderSeo() }
+				{ this.renderCopyPost() }
 				{ this.renderMoreOptions() }
 			</div>
 		);
